7ca373cc46fffaccd5f5abf6ef3395b597c0d41b
Jonas <jonas+git_public@joba.me>
2023-11-30 19:53:22 +0100

    Better request recording

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -14,40 +14,26 @@ pub type Connection = r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManage
 
 pub fn record_request(conn: &Connection, scramble: &Algorithm<Turn333>, step_configs: &Vec<StepConfig>, tables: &PruningTables333) -> rusqlite::Result<()> {
     let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("System time before unix epoch").as_secs();
-    let canonical = get_canonical_scrambles(scramble, tables);
+    let canonical = get_canonical_scramble_id(scramble);
     let encoded_steps = base64::engine::general_purpose::STANDARD.encode(serde_json::to_string(step_configs).unwrap());
     conn.execute(
-        "INSERT INTO requests (id, scramble, timestamp, canonical_scramble, canonical_scramble_inv, settings) VALUES (NULL, ?, ?, ?, ?, ?)",
+        "INSERT INTO requests (id, timestamp, scramble, canonical_scramble_id, settings) VALUES (NULL, ?, ?, ?, ?)",
         (
-            scramble.to_string(),
             time,
-            canonical.0.to_string(),
-            canonical.1.to_string(),
+            scramble.to_string(),
+            canonical,
             encoded_steps
         )
     ).map(|_|())
 }
 
-fn get_canonical_scrambles(scramble: &Algorithm<Turn333>, tables: &PruningTables333) -> (Algorithm<Turn333>, Algorithm<Turn333>) {
-    let step_config = build_steps(vec![
-            StepConfig::new(StepKind::EO),
-            StepConfig::new(StepKind::DR),
-            StepConfig::new(StepKind::HTR),
-            StepConfig::new(StepKind::FR),
-            StepConfig::new(StepKind::FIN),
-    ].into_iter().map(|mut s|{
-        s.quality = 1;
-        s.niss = Some(NissSwitchType::Never);
-        s.step_limit = Some(1);
-        s
-    }).collect(), tables).unwrap();
+fn get_canonical_scramble_id(scramble: &Algorithm<Turn333>) -> String {
     let mut cube = Cube333::default();
     cube.apply_alg(scramble);
-    let mut cube_inverse = cube.clone();
-    cube_inverse.invert();
-    let canonical_normal = solve_steps(cube, &step_config, CancellationToken::new()).next().expect("Expected solution");
-    let canonical_inverse = solve_steps(cube_inverse, &step_config, CancellationToken::new()).next().expect("Expected solution");
-    (canonical_normal.into(), canonical_inverse.into())
+    let normal_encoded = serialize_cube_to_base64(&cube);
+    cube.invert();
+    let inverse_encoded = serialize_cube_to_base64(&cube);
+    std::cmp::min(normal_encoded, inverse_encoded)
 }
 
 pub fn init_db(conn: &Connection) -> rusqlite::Result<usize> {
@@ -60,12 +46,20 @@ fn create_requests_table(conn: &Connection) -> rusqlite::Result<usize> {
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             scramble TEXT NOT NULL,
             timestamp INTEGER NOT NULL,
-            canonical_scramble TEXT NOT NULL,
-            canonical_scramble_inv TEXT NOT NULL,
+            canonical_scramble_id TEXT NOT NULL,
             settings TEXT NOT NULL
         )
     ", [])?;
     conn.execute("CREATE INDEX IF NOT EXISTS id ON requests (scramble)", [])?;
-    conn.execute("CREATE INDEX IF NOT EXISTS id ON requests (canonical_scramble)", [])?;
-    conn.execute("CREATE INDEX IF NOT EXISTS id ON requests (canonical_scramble_inv)", [])
+    conn.execute("CREATE INDEX IF NOT EXISTS id ON requests (canonical_scramble_id)", [])?;
+    conn.execute("CREATE INDEX IF NOT EXISTS id ON requests (canonical_scramble_inv_id)", [])
+}
+
+fn serialize_cube_to_base64(cube: &Cube333) -> String {
+    let mut cube_longs = cube.edges.get_edges_raw().to_vec();
+    cube_longs.push(cube.corners.get_corners_raw());
+    let bytes: Vec<u8> = cube_longs.into_iter()
+        .flat_map(|x| x.to_le_bytes().into_iter())
+        .collect();
+    base64::engine::general_purpose::STANDARD.encode(&bytes)
 }

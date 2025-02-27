e2b8de7276ddd68b8a11d5e29514320642359053
Jonas <jonas+git_public@joba.me>
2025-02-27 11:01:26 +0100

    Finish step fix

--- cubelib/src/main.rs
+++ cubelib/src/main.rs
@@ -2,12 +2,12 @@ use std::str::FromStr;
 
 use cubelib::algs::Algorithm;
 use cubelib::puzzles::c333::Cube333;
-use cubelib::puzzles::c333::steps::fr::coords::FRUDNoSliceCoord;
+use cubelib::puzzles::c333::steps::finish::coords::HTRFinishCoord;
 use cubelib::puzzles::puzzle::ApplyAlgorithm;
 
 pub fn main() {
     let mut cube = Cube333::default();
-    let alg = Algorithm::from_str("U2").unwrap();
+    let alg = Algorithm::from_str("U2 F2 D2 L2 R2 B2").unwrap();
     cube.apply_alg(&alg);
-    println!("{:?}", FRUDNoSliceCoord::from(&cube));
+    println!("{:?}", HTRFinishCoord::from(&cube));
 }
--- cubelib/src/puzzles/c333/steps/finish/coords.rs
+++ cubelib/src/puzzles/c333/steps/finish/coords.rs
@@ -160,6 +160,7 @@ mod avx2 {
             -1, 4,-1, 5,
             -1, 3,-1, 0,
             -1, 1,-1, 2));
+
         let binom = _mm_mullo_epi16(sum, _mm_set_epi16(0, 0, 0, 0, 24, 6, 2, 1));
         let full_sum = _mm_hadd_epi16(_mm_hadd_epi16(_mm_hadd_epi16(binom, _mm_set1_epi8(0)), _mm_set1_epi8(0)), _mm_set1_epi8(0));
         let cp_eep_value = _mm_extract_epi16::<0>(full_sum) as u16;
@@ -229,6 +230,11 @@ mod avx2 {
             -1, 4, 4, 4));
         let higher_left_e12 = _mm_and_si128(_mm_cmplt_epi8(values_e12, cmp_values), _mm_set1_epi8(1));
 
+        println!("{higher_left_246:?}");
+        println!("{values_e12:?}");
+        println!("{cmp_values:?}");
+        println!("{higher_left_e12:?}");
+
         let sum = _mm_hadd_epi32(higher_left_246, higher_left_e12);
         let sum = _mm_hadd_epi32(sum, _mm_set1_epi8(0));
         let sum = _mm_or_si128(sum, _mm_shuffle_epi8(orbit_corners, _mm_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1)));
@@ -237,6 +243,8 @@ mod avx2 {
             -1, 4,-1, 5,
             -1, 3,-1, 0,
             -1, 1,-1, 2));
+
+        println!("{sum:?}");
         let binom = _mm_mullo_epi16(sum, _mm_set_epi16(0, 0, 3, 1, 24*12, 6*12, 2*12, 1*12));
         let full_sum = _mm_hadd_epi16(_mm_hadd_epi16(_mm_hadd_epi16(binom, _mm_set1_epi8(0)), _mm_set1_epi8(0)), _mm_set1_epi8(0));
         let cp_eep_value = _mm_extract_epi16::<0>(full_sum) as u32;
@@ -340,12 +348,12 @@ mod neon {
         let values_246 = vtbl1_u8(orbit_corners, C8{ a_i8: [2, 4, 6, 4, 6, 6, -1, -1]}.a);
         let higher_left_246 = vand_u8(vclt_u8(values_246, vtbl1_u8(orbit_corners, C8{ a_i8: [0, 0, 0, 2, 2, 4, -1, -1]}.a)), vdup_n_u8(1));
 
-        let values_e12 = vtbl1_u8(orbit_corners, C8{ a_i8: [5, 6, 7, 6, 7, 7, -1, -1]}.a);
+        let values_e12 = vqtbl1_u8(edges, C8{ a_i8: [5, 6, 7, 6, 7, 7, -1, -1]}.a);
         let higher_left_e12 = vand_u8(vclt_u8(values_e12, vqtbl1_u8(edges, C8{ a_i8: [4, 4, 4, 5, 5, 6, -1, -1]}.a)), vdup_n_u8(1));
 
         let combined = vcombine_u8(higher_left_246, higher_left_e12);
-        let sum = vaddq_u8(combined, vqtbl1q_u8(combined, C16{ a_i8: [0, 3, 4, -1, -1, -1, -1, -1, 8, 11, 12, -1, -1, -1, -1, -1]}.a));
-        let sum = vaddq_u8(sum, vqtbl1q_u8(sum, C16{ a_i8: [0, 1, 5, -1, -1, -1, -1, -1, 8, 9, 13, -1, -1, -1, -1, -1]}.a));
+        let sum = vaddq_u8(combined, vqtbl1q_u8(combined, C16{ a_i8: [-1, 3, 4, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1, -1]}.a));
+        let sum = vaddq_u8(sum, vqtbl1q_u8(sum, C16{ a_i8: [-1, 1, 5, -1, -1, -1, -1, -1, -1, 9, 13, -1, -1, -1, -1, -1]}.a));
         let sum = vqtbl1q_u8(sum, C16{ a_i8: [0, -1, 1, 2, 8, 9, 10, -1, -1, -1, -1, -1, -1, -1, -1, -1]}.a);
         let sum = vorrq_u8(sum, vcombine_u8(vand_u8(orbit_corners, C8{ a_i8: [0, -1, 0, 0, 0, 0, 0, 0]}.a), vdup_n_u8(0)));
         let sum = vreinterpretq_u16_u8(vzip1q_u8(sum, vdupq_n_u8(0)));
@@ -356,8 +364,8 @@ mod neon {
         let cmp_values = vqtbl1q_u8(edges, C16{ a_i8:      [0, 0,  0, 2,  2,  8, -1, -1, 1, 1,  1, 3,  3,  9, -1, -1]}.a);
         let higher_left_m123s123 = vandq_u8(vcltq_u8(values_m123s123, cmp_values), vdupq_n_u8(1));
 
-        let sum = vaddq_u8(higher_left_m123s123, vqtbl1q_u8(higher_left_m123s123, C16{ a_i8: [0, 3, 4, -1, -1, -1, -1, -1, 8, 11, 12, -1, -1, -1, -1, -1]}.a));
-        let sum = vaddq_u8(sum, vqtbl1q_u8(sum, C16{ a_i8: [0, 1, 5, -1, -1, -1, -1, -1, 8, 9, 13, -1, -1, -1, -1, -1]}.a));
+        let sum = vaddq_u8(higher_left_m123s123, vqtbl1q_u8(higher_left_m123s123, C16{ a_i8: [-1, 3, 4, -1, -1, -1, -1, -1, -1, 11, 12, -1, -1, -1, -1, -1]}.a));
+        let sum = vaddq_u8(sum, vqtbl1q_u8(sum, C16{ a_i8: [-1, 1, 5, -1, -1, -1, -1, -1, -1, 9, 13, -1, -1, -1, -1, -1]}.a));
 
         let sum = vreinterpretq_u16_u8(vqtbl1q_u8(sum, C16{ a_i8: [0, -1, 1, -1, 2, -1, 8, -1, 9, -1, 10, -1, -1, -1, -1, -1]}.a));
         let binom = vmulq_u16(sum, C16{ a_u16: [6, 2, 1, 6*24, 2*24, 1*24, 0, 0]}.a_16);
